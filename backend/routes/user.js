9bdf2585b4a2d6d30ad84a8c5da103dceaad3bc0
Gentalmang <asgrrem98@gmail.com>
2022-03-08 21:48:10 -0400

    auth middleware done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -26,6 +28,45 @@ router.post("/signup", (req, res, next) => {
       });
   });
 
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  //first find if the email address exist
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if (!user){
+        return res.status(401)
+          .json({
+            message: "Auth failed"
+          });
+      }
+      fetchedUser = user;
+      //check if the password is correct
+      return bcrypt.compare(req.body.password, user.password);
+    })
+      .then(result => {
+        //if not, return an error
+        if(!result){
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        //if yes, create the web token, read more at jwt.io
+        const token = jwt.sign(
+          { email: fetchedUser.email, userId: fetchedUser._id },
+          'secret_this_should_be_longer',
+          {expiresIn: "1h"}
+        );
+        res.status(200).json({
+          token: token
+        });
+      })
+      .catch(err => {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      });
 });
 
 module.exports = router;
